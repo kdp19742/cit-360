269ec72d2bde3a4fef4562fb366ab40856ae87ab
Kean Parto <keanparto@Keans-MacBook-Pro.local>
2016-11-23 16:51:51 -0800

    update

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -200,12 +200,12 @@ resource "aws_security_group" "web" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["172.31.0.0/16"]
   }
 
   egress {
     from_port = 0
-    to_port= 0
+    to_port = 0
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -237,6 +237,13 @@ resource "aws_security_group" "db" {
     to_port = 3306
     protocol = "tcp"
     cidr_blocks =["172.31.0.0/16"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
